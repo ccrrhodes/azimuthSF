138f9567fc9d841427116ac9cb3075947fb47bda
ccrrhodes <166561061+ccrrhodes@users.noreply.github.com>
2025-05-14 23:52:03 -0400

    Update ui.R

--- R/ui.R
+++ R/ui.R
@@ -52,7 +52,7 @@ AzimuthUI <- tagList(
       ),
       sidebarMenu(
         menuItem("Widgets", tabName = "widgets", icon = icon("th"))
-      )
+      ),
       shiny::textInput("file11", "File Path", "Data Summary"),
       actionButton("button11", "GO"),
       div(
